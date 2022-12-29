29e37db759f3bbddfefe57a0c36f9d9d0e74821f
matdev93 <112671782+matdev93@users.noreply.github.com>
2022-12-29 17:09:46 -0300

    Insert Api en tarjeta principal

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-console.log('hola, funciona');
-console.log('hola otra vez, funciona');
+let weather = {
+    "apikey": "45ef79a15574fd2f0643500a864f1492",
+    fetchWeather: function (city) {
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apikey)
+        
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, temp_min, temp_max } = data.main;
+        console.log(name, icon, description, temp, temp_min, temp_max);
+        document.querySelector(".info-dia0").innerText = name;
+        document.querySelector("icon0").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".degree-dia0").innerText = temp;
 
-console.log('prueba');
-console.log("HOLA");
-console.log("NUeva prueba")
+    }
+}
 
